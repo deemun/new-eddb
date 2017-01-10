445eaed002d8c37230795e387f9546ba99f239d7
deemun <sio_lab@siodigitalsair.bcricwh.lan>
2017-01-10 12:54:33 -0800

    input box added to tabs

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -9,6 +9,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 export class AppTab implements OnInit {
   public tabs =  ['Tab1', 'Tab2', 'Tab3', 'Tab4'];
- 
+  public tabInput: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+
+  createTab(tabInput) {
+    if (tabInput.isValid) {
+      
+    }
+    
+  }
+
+  ngOnInit() {
+    console.log('This is call before initalization');
+    // Definig fb type.
+    // FormGroup is collection of inputs
+    // FormBuilder is use to put different collections of input together.
+    this.tabInput = this.fb.group({
+      tabValue: ['', Validators.required]
+    });
+  }
 }
 
